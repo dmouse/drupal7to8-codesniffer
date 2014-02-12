f3ec0af10535c8b889df3e13615c35e7d2aff4b5
Lisa Backer <lisa.backer@acquia.com>
2014-02-12 18:09:09 -0500

    Add logic for variable_set and variable_del

--- Drupal7to8/Sniffs/VariableAPI/VariableAPISniff.php
+++ Drupal7to8/Sniffs/VariableAPI/VariableAPISniff.php
@@ -24,14 +24,8 @@ class Drupal7to8_Sniffs_VariableAPI_VariableAPISniff extends Drupal7to8_Base_Fun
 
   protected $forbiddenFunctions = array(
     'variable_get' => '\Drupal::config()->get()',
-    'variable_set' => NULL,
-    'variable_del' => NULL,
-  );
-
-  protected $dynamicArgumentReplacements = array(
-    'variable_get' => array(
-      'arguments' => array(1),
-    ),
+    'variable_set' => '\Drupal::config()->set()',
+    'variable_del' => '\Drupal::config()->clear()',
   );
 
   /**
@@ -54,30 +48,59 @@ class Drupal7to8_Sniffs_VariableAPI_VariableAPISniff extends Drupal7to8_Base_Fun
       // dynamically build the replacement string for this function call.
       switch ($function) {
         case 'variable_get':
-          $result = $this->findNthArgument($phpcsFile, $stackPtr, 0);
-          // Return early if there is no variable to get.
-          if ($result === FALSE) {
-            continue;
-          }
-          // Find the variable name to get
-          list($arg_start, $arg_end, $remove_start, $remove_end) = $result;
-          $varname = $this->getContentForTokenRange($tokens, $arg_start, $arg_end);
-
           // Convert to the new config object name and updated variable name.
-          list($config_object_name, $updated_varname) = $this->updateVariableName($varname, $phpcsFile);
+          $result = $this->getUpdatedVariableName($phpcsFile, $stackPtr, $tokens);
+          if ($result === FALSE) {
+            return;
+          }
+          list($config_object_name, $updated_varname) = $result;
 
           // Update to the new statement.
-          $replacement = "\Drupal::config('" . $config_object_name . "')->get('" . $updated_varname . "')";
+          $replacement = "\Drupal::config('$config_object_name')->get($updated_varname)";
 
-          // Remove the rest of the function tokens.
-          $openParenthesis = $phpcsFile->findNext(PHP_CodeSniffer_Tokens::$emptyTokens, ($stackPtr + 1), NULL, TRUE);
-          $closeParenthesis = $tokens[$openParenthesis]['parenthesis_closer'];
-          $this->removeTokenRange($phpcsFile->fixer, $openParenthesis, $closeParenthesis);
-          $phpcsFile->fixer->replaceToken($stackPtr, $replacement);
+          // Remove the original function arguments and add replacement.
+          $this->updateFunctionSignature($phpcsFile, $stackPtr, $tokens, $replacement);
+          break;
+
+        case 'variable_set':
+          // Get the variable argument
+          $result = $this->getUpdatedVariableName($phpcsFile, $stackPtr, $tokens);
+          if ($result === FALSE) {
+            return;
+          }
+          list($config_object_name, $updated_varname) = $result;
+
+          // Find the updated value
+          $result = $this->findNthArgument($phpcsFile, $stackPtr, 1);
+          // Return early if there is no value to set.
+          if ($result === FALSE) {
+            return;
+          }
+          list($arg_start, $arg_end, $remove_start, $remove_end) = $result;
+          $new_value = Drupal7to8_Utility_TokenRange::getContent($tokens, $arg_start, $arg_end);
+
+          // Update to the new statement.
+          $replacement = "\Drupal::config('$config_object_name')->set($updated_varname, $new_value)->save()";
+
+          // Remove the original function arguments and add replacement.
+          $this->updateFunctionSignature($phpcsFile, $stackPtr, $tokens, $replacement);
+          break;
+
+        case 'variable_del':
+          // Convert to the new config object name and updated variable name.
+          $result = $this->getUpdatedVariableName($phpcsFile, $stackPtr, $tokens);
+          if ($result === FALSE) {
+            return;
+          }
+          list($config_object_name, $updated_varname) = $result;
+
+          // Update to the new statement.
+          $replacement = "\Drupal::config('$config_object_name')->clear($updated_varname)->save()";
+
+          // Remove the original function arguments and add replacement.
+          $this->updateFunctionSignature($phpcsFile, $stackPtr, $tokens, $replacement);
           break;
       }
-
-      $this->insertUseStatement($phpcsFile, $function, $pattern);
     }
     elseif ($fix === FALSE) {
       $phpcsFile->addError($message, $stackPtr, $this->code);
@@ -85,17 +108,58 @@ class Drupal7to8_Sniffs_VariableAPI_VariableAPISniff extends Drupal7to8_Base_Fun
   }
 
   /**
-   * Update the variable name used to conform to the new settings format.
-   * @param $varname
+   * Get the original variable name and convert it to conform to the new format.
+   *
+   * @param PHP_CodeSniffer_File $phpcsFile
+   *   The file object being updated.
+   * @param $stackPtr
+   *   The token index for the variable function.
+   * @param $tokens
+   *   An array of parsed tokens within the file.
    */
-  function updateVariableName($varname, $phpcsFile) {
-    // replace single quotes
-    $varname = str_replace("'", "", $varname);
+  function getUpdatedVariableName(PHP_CodeSniffer_File $phpcsFile, $stackPtr, $tokens) {
+    $result = $this->findNthArgument($phpcsFile, $stackPtr, 0);
+    // Return early if there is no variable to get.
+    if ($result === FALSE) {
+      return FALSE;
+    }
+    // Find the variable name to get
+    list($arg_start, $arg_end, $remove_start, $remove_end) = $result;
+    $varname = Drupal7to8_Utility_TokenRange::getContent($tokens, $arg_start, $arg_end);
+
+    // Convert to the new config object name and updated variable name.
+    $cleaned_varname = str_replace("'", "", $varname);
     $module_name = Drupal7to8_Utility_ModuleProperties::getModuleName($phpcsFile);
-    $var_parts = explode('_', $varname);
+    $var_parts = explode('_', $cleaned_varname);
     if ($var_parts[0] == $module_name) {
       array_shift($var_parts);
     }
-    return array($module_name . '.setting', implode('_', $var_parts));
+    $updated_varname = implode('_', $var_parts);
+    if (strlen($cleaned_varname) !== strlen($varname)) {
+      // If the original name was a string literal, then return the same.
+      $updated_varname = "'" . $updated_varname . "'";
+    }
+    return array($module_name . '.setting', $updated_varname);
+  }
+
+  /**
+   * Update the entire function signature.
+   *
+   * Remove the original argument tokens and add the new function signature.
+   *
+   * @param PHP_CodeSniffer_File $phpcsFile
+   *   The file object being updated.
+   * @param $stackPtr
+   *   The token index for the variable function.
+   * @param $tokens
+   *   An array of parsed tokens within the file.
+   * @param $replacement
+   *   The new function string containing the function name and arguments.
+   */
+  function updateFunctionSignature(PHP_CodeSniffer_File $phpcsFile, $stackPtr, $tokens, $replacement) {
+    $openParenthesis = $phpcsFile->findNext(PHP_CodeSniffer_Tokens::$emptyTokens, ($stackPtr + 1), NULL, TRUE);
+    $closeParenthesis = $tokens[$openParenthesis]['parenthesis_closer'];
+    Drupal7to8_Utility_TokenRange::remove($phpcsFile->fixer, $openParenthesis, $closeParenthesis);
+    $phpcsFile->fixer->replaceToken($stackPtr, $replacement);
   }
 }
