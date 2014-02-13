08a701ca79c208f1195a0477b239b6bfb3cec54c
Gabor Hojtsy <gabor@hojtsy.hu>
2014-02-13 15:46:04 -0500

    Issue #2195763 basics for theme()

---
+++ Drupal7to8/Sniffs/Theme/ThemeSniff.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Drupal7to8_Sniffs_Theme_ThemeSniff.
+ *
+ * PHP version 5
+ *
+ * @category PHP
+ * @package  PHP_CodeSniffer
+ * @link     http://pear.php.net/package/PHP_CodeSniffer
+ */
+
+/**
+ * theme() is deprecated.
+ *
+ * @category PHP
+ * @package  PHP_CodeSniffer
+ * @link     http://pear.php.net/package/PHP_CodeSniffer
+ */
+class Drupal7to8_Sniffs_Theme_ThemeSniff extends Drupal7to8_Base_FunctionReplacementSniff {
+
+    protected $message = 'Instead of directly calling !function(), use render arrays: https://drupal.org/node/2195739';
+
+    protected $code = 'Theme';
+
+    // Don't replace with _theme() because that is discouraged too.
+    protected $forbiddenFunctions = array(
+        'theme' => NULL,
+    );
+
+}
