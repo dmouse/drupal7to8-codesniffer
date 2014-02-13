846ab1be59000b5575de3146b3975f2c466c3287
Gabor Hojtsy <gabor@hojtsy.hu>
2014-02-13 14:28:32 -0500

    Issue #2194833 followup: more language API coverage, but these will only be fixme_-d

--- Drupal7to8/Sniffs/Language/LanguageAPISniff.php
+++ Drupal7to8/Sniffs/Language/LanguageAPISniff.php
@@ -23,10 +23,43 @@ class Drupal7to8_Sniffs_Language_LanguageAPISniff extends Drupal7to8_Base_Functi
     protected $code = 'LanguageAPI';
 
     protected $forbiddenFunctions = array(
+        // Functions with easy replacements on the language manager. Although
+        // some places may have local instances of the language manager, that is
+        // impossible to tell here.
         'language' => '\Drupal::languageManager()->getCurrentLanguage',
         'language_list' => '\Drupal::languageManager()->getLanguages',
         'language_load' => '\Drupal::languageManager()->getLanguage',
         'language_default' => '\Drupal::languageManager()->getDefaultLanguage',
+
+        // Misc language API functions without direct replacements (or requiring
+        // instances of negotiators.
+        'language_negotiation_get_switch_links' => NULL,
+        'language_types_info' => NULL,
+        'language_types_get_configurable' => NULL,
+        'language_types_disable' => NULL,
+        'language_update_locked_weights' => NULL,
+        'language_types_get_all' => NULL,
+        'language_types_set' => NULL,
+        'language_types_initialize' => NULL,
+        'language_negotiation_method_get_first' => NULL,
+        'language_negotiation_method_enabled' => NULL,
+        'language_negotiation_purge' => NULL,
+        'language_negotiation_set' => NULL,
+        'language_negotiation_info' => NULL,
+        'language_negotiation_method_invoke' => NULL,
+        'language_url_split_prefix' => NULL,
+
+        // Data derivatives that require instances of language negotiators.
+        'language_from_selected' => NULL,
+        'language_from_browser' => NULL,
+        'language_from_user' => NULL,
+        'language_from_user_admin' => NULL,
+        'language_from_session' => NULL,
+        'language_from_url' => NULL,
+        'language_url_fallback' => NULL,
+        'language_switcher_session' => NULL,
+        'language_switcher_url' => NULL,
+        'language_url_rewrite_session' => NULL,
     );
 
     /**
